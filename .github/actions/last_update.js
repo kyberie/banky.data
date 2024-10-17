22d350846ba0d4a80716a70ed5d303547c18435d
Milan Somsedík <milan.somsedik@gmail.com>
2024-10-17 14:06:54 +0200

    loga + update script

--- .github/actions/last_update.js
+++ .github/actions/last_update.js
@@ -5,14 +5,7 @@ const archiver = require('archiver');
 
 fs.readFile('main/data.json', (err, data) => {
   let banky = JSON.parse(data);
-  let zip_name_all = banky.logos_zip_file_all;
   let zip_name_domov = banky.logos_zip_file_domov;
-
-  var outputAll = fs.createWriteStream('main/' + zip_name_all);
-  var archiveAll = archiver('zip', {
-    zlib: { level: 9 } 
-  });
-  archiveAll.pipe(outputAll);
 
   var outputDomov = fs.createWriteStream('main/' + zip_name_domov);
   var archiveDomov = archiver('zip', {
@@ -20,14 +13,9 @@ fs.readFile('main/data.json', (err, data) => {
   });
   archiveDomov.pipe(outputDomov);
 
-  var logos = [];
   var logosDomov = [];
   for (index in banky.banks) {
     var bank = banky.banks[index];
-    if (logos[bank.logo] != 1) {
-      archiveAll.file('main/logo/' + bank.logo, { name: bank.logo });
-      logos[bank.logo] = 1;
-    }
 	
 	if (bank.app_domov.use_in_app) {
       if (logosDomov[bank.logo] != 1) {
@@ -39,7 +27,6 @@ fs.readFile('main/data.json', (err, data) => {
   //  console.log(bank.logo);
   }
 
-  archiveAll.finalize();
   archiveDomov.finalize();
 });
 
